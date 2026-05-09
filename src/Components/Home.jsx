cf884d5fcf827e1dcb1c60c7b1a8c1e99bfb9117
Farouq Olaleye <farouqolamilekan2006@gmail.com>
2026-05-09 17:24:58 +0100

    first commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -14,9 +14,9 @@ const Home = () => {
             <h3 className="text-[#9B8876] text-sm font-sans mt-4">An architect-led design-build studio crafting homes, <br /> commercial spaces, and public buildings that honour Nigerian <br /> culture and define its future.</h3>
             <button className="text-white text-lg mt-10 bg-[#C4622D] px-14 h-16 rounded-full hover:bg-white hover:text-[#C4622D] cursor-pointer transition-all duration-300">View Our Projects </button>
         </div>
-    <div className="w-full max-w-4xl mx-auto">
+    {/* <div className="w-full max-w-4xl mx-auto">
         <img src={hero} alt="" />
-    </div>
+    </div> */}
             
   
         </div>
